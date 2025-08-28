22931d4b6e48432377a55a66ce4223fb90b1acb4
Polat Kargılı <54178145+aliora@users.noreply.github.com>
2025-08-28 11:30:34 +0300

    a

--- relay_control-main/main.py
+++ relay_control-main/main.py
@@ -9,11 +9,6 @@ def main():
     # Rl-02 Example
     relay_control.trigger_relay('127.0.0.1', 5050, relay_number=3, duration=100)
 
-    # CH340 USB Relay Example
-    print("\n--- CH340 USB Röle Testi ---")
-    ch340_relay = RelayControl('ch340')
-    ch340_relay.trigger_relay(relay_number=1)  # IP ve port gerekmez
-
 
 if __name__ == "__main__":
     main()
---
+++ relay_control-main/relay_control.py
@@ -0,0 +1,26 @@
+from .models.Rl02_IO import Rl02IO
+from .models.Rn62_IO import Rn62IO
+from .models.Jetson_Embed import JetsonEmbed
+from .models.Raspberry_Embed import RaspberryEmbed
+from .models.Desktop_Embed import DesktopEmbed
+
+
+class RelayControl:
+    def __init__(self, brand):
+        self.brand = brand
+
+        if self.brand == 'rl-02':
+            self.relay_instance = Rl02IO()
+        elif self.brand == 'rn-62':
+            self.relay_instance = Rn62IO()
+        elif self.brand == 'jetson-embed':
+            self.relay_instance = JetsonEmbed()
+        elif self.brand == 'raspberry-embed':
+            self.relay_instance = RaspberryEmbed()
+        elif self.brand == 'desktop-embed':
+            self.relay_instance = DesktopEmbed()
+        else:
+            raise ValueError("Unsupported brand for relay control")
+
+    def trigger_relay(self, ip, port, relay_number=None, duration=100):
+        return self.relay_instance.trigger_relays(ip, port, relay_number, duration)
